1cf77772ab5c373fbbfffe6f1630e079cef8c834
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2025-04-18 21:44:08 +0530

    Update check_a_number_is_palindrome_or_not.py

--- check_a_number_is_palindrome_or_not.py
+++ check_a_number_is_palindrome_or_not.py
@@ -11,3 +11,25 @@ if total == num:
 else:
     print(f"{num} is not a palindrome number")
 
+
+#______________________________________________
+num = int(input("Enter a number: "))
+temp = num
+new_num = 0
+count = 0
+while temp > 0:
+    count += 1
+    temp = temp // 10
+
+temp = num
+while temp > 0:
+    rem = temp % 10
+    new_num += rem * (10 ** (count - 1))
+    count -= 1
+    temp = temp // 10
+
+if new_num == num:
+    print(f"{num} is a palindrome number.")
+else:
+    print(f"{num} is not a palindrome number.")
+
