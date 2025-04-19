e681330efc9187d568d21638bb6415d4fe0fd0bf
Sayantan Golder <sayantangolder1710@gmail.com>
2025-04-19 07:56:34 +0530

    New Commit

---
+++ diamond_shaped_pyramid.py
@@ -0,0 +1,14 @@
+rows = int(input("Enter the number of rows you want: "))
+for i in range(1, ((rows + 1) // 2) + 1):
+    for j in range(rows - i):
+        print(" ", end="")
+    for k in range(i):
+        print("* ", end="")
+    print("")
+for i in range(((rows + 1) // 2) - 1, 0, -1):
+    for j in range(rows - i):
+        print(" ", end="")
+    for k in range(i):
+        print("* ", end="")
+    print("")
+
