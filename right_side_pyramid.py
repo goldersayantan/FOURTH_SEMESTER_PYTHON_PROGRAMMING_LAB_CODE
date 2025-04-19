662deffa9caa175119f766819db31b6f52476ad5
Sayantan Golder <sayantangolder1710@gmail.com>
2025-04-19 07:39:25 +0530

    New Commit

---
+++ right_side_pyramid.py
@@ -0,0 +1,7 @@
+rows = int(input("Enter the number of rows you want: "))
+for i in range(1, rows + 1):
+    for j in range(rows - i):
+        print(" ", end="")
+    for k in range(i):
+        print("* ", end="")
+    print("")
