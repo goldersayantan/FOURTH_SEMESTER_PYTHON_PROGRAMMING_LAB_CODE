82b258621ee71fffd1f3e2c26921b8557da445da
Sayantan Golder <sayantangolder1710@gmail.com>
2025-04-19 07:40:32 +0530

    New Commit

--- right_side_pyramid.py
+++ right_side_pyramid.py
@@ -3,5 +3,5 @@ for i in range(1, rows + 1):
     for j in range(rows - i):
         print(" ", end="")
     for k in range(i):
-        print("* ", end="")
+        print("*", end="")
     print("")
